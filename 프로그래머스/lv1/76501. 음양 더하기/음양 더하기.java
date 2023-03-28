fd39e1b077ac1d3e57877e5b0fd375427e1101b5
Haeun Noh <okhagrace06@naver.com>
2023-03-29 00:06:57 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        //int answer = 123456789;
+        int sum = 0;
+        for ( int i = 0; i < absolutes.length; i++ ) {
+            sum += (signs[i] == true ? absolutes[i] : -absolutes[i]);
+        }
+        
+        return sum;
+    }
+}
