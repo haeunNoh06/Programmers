9ff199564cd19bafdcd88b8b183bd89657ca0e3d
Haeun Noh <okhagrace06@naver.com>
2023-04-01 10:57:26 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.15 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,14 +1,8 @@
 class Solution {
     public String solution(int n) {
         String answer = "";
-        String[] result = new String[n];
-        
-        for ( int i = 0; i < n; i++ ) {
-            result[i] = ( i%2 == 0 ? "수": "박");
-        }
-        answer = String.join("", result);   //합친 문자열 반환
-        //첫 번째 인자로 배열의 원소간 연결할 구분 문자, 두 번째 인자로 배열을 넣습니다.
-        
+        for (int i = 1; i <= n; i++)
+            answer += ( i % 2 == 0 ? "박" : "수");
         return answer;
     }
 }
