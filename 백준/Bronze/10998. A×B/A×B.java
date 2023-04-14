3089de320d7095afd5180135cd7eb024c93cf7a6
Haeun Noh <okhagrace06@naver.com>
2023-04-14 09:32:05 +0900

    [Bronze V] Title: A×B, Time: 212 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int num;
+        num = scan.nextInt();
+        System.out.println(num*scan.nextInt());
+    }
+}
