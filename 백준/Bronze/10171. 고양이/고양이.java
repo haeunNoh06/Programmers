57ba96ba4717f22443d4ec258a4ab78b258795e4
Haeun Noh <okhagrace06@naver.com>
2023-04-14 09:49:14 +0900

    [Bronze V] Title: 고양이, Time: 112 ms, Memory: 13852 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n )  ( \')\n(  /  )\n \\(__)|");
+        /*
+        System.out.println(" )   ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");       
+        */
+    }
+}
