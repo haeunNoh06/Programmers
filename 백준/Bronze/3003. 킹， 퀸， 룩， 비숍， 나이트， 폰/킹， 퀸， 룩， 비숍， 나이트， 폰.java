ca6f8fb71cc62b782543c18921aa569abaa0a6d3
Haeun Noh <okhagrace06@naver.com>
2023-04-16 21:28:54 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 268 ms, Memory: 19212 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String [] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int[] num = new int[6];
+        for (int i = 0; i < num.length; i++) {
+            num[i] = scan.nextInt();
+        }
+        
+        System.out.println((1-num[0])+" "+(1-num[1])+" "+(2-num[2])+" "+(2-num[3])+" "+(2-num[4])+" "+(8-num[5]));
+    }
+}
