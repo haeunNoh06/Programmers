09d0d97f1737a1b8cb33bd26dcb89a6915e01a7d
Haeun Noh <okhagrace06@naver.com>
2023-04-16 21:10:28 +0900

    [Bronze V] Title: 구구단, Time: 236 ms, Memory: 18540 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int A = scan.nextInt();
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(A+" * "+i+" = "+(A*i));
+        }
+    }
+}
