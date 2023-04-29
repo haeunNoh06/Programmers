ec3eb0fdac20597b921ffa8ed3bdab479c985bcf
Haeun Noh <okhagrace06@naver.com>
2023-04-29 23:04:38 +0900

    [unrated] Title: 소문자로 바꾸기, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -1,12 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        for ( int i = 0; i < myString.length(); i++ ) {
-            char ch = myString.charAt(i);
-            if ( ch < 97 )
-                ch += 32;
-            answer += ch;
-        }
-        return answer;
+        return myString.toLowerCase();
     }
 }
