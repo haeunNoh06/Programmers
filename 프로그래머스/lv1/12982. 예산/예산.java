7094d8ddbfd9cf9df5553f539fbf386a0a1e1850
Haeun Noh <okhagrace06@naver.com>
2023-05-08 17:55:18 +0900

    [level 1] Title: 예산, Time: 0.35 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.java
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -1,20 +1,7 @@
 import java.util.Arrays;
-
 class Solution {
     public int solution(int[] d, int budget) {
-        
-        // 정렬
-        //Arrays.sort(d);
-        for ( int i = 0; i < d.length; i++) {
-            for ( int j = 1; j < d.length-i; j++) {
-                if (d[j-1] > d[j]) {
-                    int tmp = d[j-1];
-                    d[j-1] = d[j];
-                    d[j] = tmp;
-                }
-            }
-        }
-        
+        Arrays.sort(d);
         int sum = 0, cnt = 0;
         for ( int i = 0; i < d.length; i++) {
             sum += d[i];
