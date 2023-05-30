281fd5b4dd0623541bdbdb103dccce5c2717a11d
Haeun Noh <okhagrace06@naver.com>
2023-05-30 15:29:45 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 13.54 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,16 +1,10 @@
 class Solution {
-	public String solution(int[] food) {
-		String me = "", you = "";
-		
-		for ( int i = 1; i < food.length; i++ ) {
-			if ( food[i] <= 1 )
-				continue;
-			for ( int j = 1; j <= food[i]/2; j++ ) {
-				me += Integer.toString(i);
-				you = Integer.toString(i) + you;
-			}
-		}
-
-		return me+"0"+you;
-	}
+    public String solution(int[] food) {
+        String answer = "0";
+        for ( int i = food.length-1; i >= 0; i--) {
+            for ( int j = 0; j < food[i]/2; j++)
+                answer = i + answer + i;
+        }
+        return answer;
+    }
 }
