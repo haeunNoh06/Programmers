5a17dac582f02146dff660a5a3fbacdcd2c3121a
Haeun Noh <okhagrace06@naver.com>
2023-06-07 11:29:07 +0900

    [unrated] Title: 추억 점수, Time: 0.02 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -1,9 +1,6 @@
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
-        for ( int i = 0; i < answer.length; i++) {
-            answer[i] = 0;
-        }
         for ( int i = 0; i < name.length; i++) {
             for ( int j = 0; j < photo.length; j++) {
                 for ( int k = 0; k < photo[j].length; k++) {
