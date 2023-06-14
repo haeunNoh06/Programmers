3a10d518be2de0cebdcc183797099a6b47d6fc80
Haeun Noh <okhagrace06@naver.com>
2023-06-14 10:47:44 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -12,38 +12,11 @@ class Solution {
         //10 31
         //11 30
         //12 31
-        /*String answer = "";
-        int month = a;
-        int day1 = b;       //누적 일 수
-        
-        for ( int i = 1; i < month; i++ ) {
-            if ( i == 2)
-                day1 += 29;
-            else if ( (i == 4) || (i == 6) || (i == 9 ) || (i == 11) )
-                day1 += 30;
-            else
-                day1 += 31;
-            
-        }
-        
-        int rest = day1 % 7;
-        switch ( rest ) {
-            case 1: answer = "FRI"; break;
-            case 2: answer = "SAT"; break;
-            case 3: answer = "SUN"; break;
-            case 4: answer = "MON"; break;
-            case 5: answer = "TUE"; break;
-            case 6: answer = "WED"; break;
-            case 0: answer = "THU";
-        }
-        
-        return answer;*/
-        
         int[] months = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30};
         String[] weekday = {"THU", "FRI", "SAT","SUN","MON","TUE","WED"};
         int days = b;
         
-        for ( int i = 0; i <a; i++) 
+        for ( int i = 1; i <a; i++) 
             days += months[i];
         
         return weekday[days%7];
