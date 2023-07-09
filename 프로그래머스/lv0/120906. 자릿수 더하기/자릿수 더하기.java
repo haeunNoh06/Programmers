35c5ac62d95b07840446d11c202d69a13ab18f1e
Haeun Noh <okhagrace06@naver.com>
2023-07-09 23:13:32 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.11 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String[] arr = Integer.toString(n).split("");
+        for ( String s : arr ) {
+            answer += Integer.parseInt(s);
+        }
+        return answer;
+    }
+}
