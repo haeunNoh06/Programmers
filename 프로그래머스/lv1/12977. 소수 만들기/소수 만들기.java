24924c6cd032a56abe6d8db6b6d50a51a0fd7051
Haeun Noh <okhagrace06@naver.com>
2023-07-19 00:41:52 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -1,44 +1,28 @@
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-
-        //주어진 수의 개수 확인
-        int cntOfNums = nums.length;
         
-        //첫째, 둘째, 셋째로 선택 된 수
-        int num1, num2, num3;
-        
-        //첫째 수 선택하는 for
-        for ( int i = 0; i < cntOfNums; i++ ) {
-            num1 = nums[i];
-            //둘째 수 선택하는 for
-            for ( int j = i+1; j < cntOfNums; j++ ) {
-                num2 = nums[j];
-                //셋째 수 선택하는 for
-                for ( int k = j+1; k < cntOfNums; k++ ) {
-                    num3 = nums[k];
-                    //선택된 3개의 수를 더한다.
-                    int sum = num1 + num2 + num3;
-                    //소수인지 확인
-                    if ( isPrime(sum)) {
+        for ( int i = 0; i < nums.length; i++) {
+            for ( int j = i+1; j < nums.length; j++) {
+                for ( int k = j+1; k < nums.length; k++) {
+                    int sum = nums[i]+nums[j]+nums[k];
+                    if ( isPrime(sum) ) {
                         answer++;
                     }
                 }
             }
         }
-        
+
         return answer;
     }
     
-    //소수를 판별하는 메서드
-    public boolean isPrime(int n) {
-        boolean result = true;
-        for ( int i = 2; i <= n/2; i++ ) {
-            if ( n % i == 0) {
-                result = false;
-                break;
+    public boolean isPrime(int num) {
+        boolean isPrime = true;
+        for ( int i = 2; i <= num/2; i++) {
+            if ( num % i == 0) {
+                isPrime = false;
             }
         }
-        return result;
+        return isPrime;
     }
-}
+} 
