f5c4916e03bba515483bc315fc97185f93c90e2f
Haeun Noh <okhagrace06@naver.com>
2023-07-26 00:18:51 +0900

    [level 1] Title: 실패율, Time: 0.06 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.java
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -1,15 +1,38 @@
-import java.util.Arrays;
+import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] answer = {};
-        int[] failPer = new int[N+2];
-        int[] player = new int[N+2];
-        Arrays.sort(stages);
-        for ( int i = 0; i < stages.length; i++) {
-            failPer[stages[i]]++;
-            player[stages[i]]++;
+        int[] failPlayer = new int[N+2];// 실패한 플레이어 수
+        int[] totalPlayer = new int[N+2];// 해당 스테이지에 도전한 플레이어 수
+        
+        // 1. 실패한 플레이어 수와 도전한 플레이어 수 구하기
+        for ( int stage : stages ) {
+            failPlayer[stage]++;
+            for ( int i = 1; i <= stage; i++) {
+                totalPlayer[i]++;
+            }
         }
-        //return failPer;
-        return player;
+        
+        Map<Integer, Double> ratesMap = new HashMap<>();// 스테이지별 실패율
+        for ( int i = 1; i <= N; i++) {
+            // 1. 스테이지별 실패한 사람의 여부를 묻고 실패율 구하고 저장
+            double failRate = ( failPlayer[i] == 0 ? 0 : (double)failPlayer[i]/totalPlayer[i] );
+            ratesMap.put(i, failRate);
+        }
+        
+        int[] answer = new int[N];
+        for ( int i = 0; i < N; i++) {
+            int maxKey = 0;
+            double maxRate = -1;
+            for ( int key : ratesMap.keySet() ) {
+                double rate = ratesMap.get(key);
+                if ( rate > maxRate ) {
+                    maxRate = rate;
+                    maxKey = key;
+                }
+            }
+            answer[i] = maxKey;
+            ratesMap.remove(maxKey);
+        }
+        return answer;
     }
 }
