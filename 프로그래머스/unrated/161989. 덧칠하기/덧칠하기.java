2d32e091309e8ed97a2863258be90dc243028845
Haeun Noh <okhagrace06@naver.com>
2023-07-26 10:07:18 +0900

    [unrated] Title: 덧칠하기, Time: 1.26 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -2,13 +2,11 @@ class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 1;
         int start = section[0];
-        
         for ( int item : section ) {
-            if ( start + m > item ) {
-                continue;
+            if ( start + m <= item ) {
+                answer++;
+                start = item;
             }
-            answer++;
-            start = item;
         }
         
         return answer;
