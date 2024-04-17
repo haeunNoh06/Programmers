9a9e58fd1a7bff329830df1cb31aa7f5c5ab4770
Haeun Noh <okhagrace06@naver.com>
2024-04-17 21:01:10 +0900

    [level 0] Title: 가위 바위 보, Time: 2.71 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        String[] arr = rsp.split("");
+        for ( int i = 0; i < arr.length; i++ ) {
+            switch(arr[i]) {
+                case "2": answer+="0"; break;
+                case "0": answer+="5"; break;
+                case "5": answer+="2"; 
+            }
+        }
+        return answer;
+    }
+}
