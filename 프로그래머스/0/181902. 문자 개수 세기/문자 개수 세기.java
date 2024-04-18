e7ba3c8cb926042600e81194fb850c9acbd5bda9
Haeun Noh <okhagrace06@naver.com>
2024-04-18 11:08:14 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -2,10 +2,7 @@ class Solution {
     public int[] solution(String my_string) {
         int[] answer = new int[52];
         for ( char c : my_string.toCharArray() ) {
-            if ( c < 97 )
-                answer[c-65]++;
-            else
-                answer[c-71]++;
+            answer[(int)(c-'A') - (c >= 'a' ? 6 : 0)]++;
         }
         return answer;
     }
