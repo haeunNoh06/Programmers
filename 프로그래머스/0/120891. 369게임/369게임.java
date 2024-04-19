75b5e8bf5ab93c9b59d223b9f63bb1e48986309f
Haeun Noh <okhagrace06@naver.com>
2024-04-19 13:10:04 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(int order) {
         int answer = 0;
         while ( order != 0 ) {
-            if ( ((order % 10)+"").contains("3") ||
-                ((order % 10)+"").contains("6") ||
-               ((order % 10)+"").contains("9"))
+            if ( (order % 10) == 3 ||
+                (order % 10) == 6 ||
+               (order % 10) == 9)
                 answer++;
             order /= 10;
         }
