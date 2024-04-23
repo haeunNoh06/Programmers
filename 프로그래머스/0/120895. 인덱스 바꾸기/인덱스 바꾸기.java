3cc48c6f7809150d84a4d496a49a96479c287f11
Haeun Noh <okhagrace06@naver.com>
2024-04-24 08:44:24 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,10 +1,8 @@
-import java.util.*;
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        String[] arr = my_string.split("");
-        String tmp = arr[num1];
-        arr[num1] = arr[num2];
-        arr[num2] = tmp;
-        return String.join("", arr);
+        char[] ch = my_string.toCharArray();
+        ch[num1] = my_string.charAt(num2);
+        ch[num2] = my_string.charAt(num1);
+        return String.valueOf(ch);
     }
 }
