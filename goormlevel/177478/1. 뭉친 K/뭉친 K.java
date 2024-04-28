816e020211e309dd663fd5d115c0625587fe1bf8
Haeun Noh <okhagrace06@naver.com>
2024-04-28 13:32:26 +0900

    [goormlevel] Title: 뭉친 K, Time: 0.26 ms, Memory: 38.75 MB, Difficulty: 3 -BaekjoonHub

--- goormlevel/177478/1. 뭉친 K/뭉친 K.java
+++ goormlevel/177478/1. 뭉친 K/뭉친 K.java
@@ -1,78 +1,57 @@
 import java.io.*;
 import java.util.*;
 import java.util.stream.*;
-
 class Main {
-	static class Pair {
-		int x;
-		int y;
-		Pair(int x, int y) {
+	static class Point {
+		int x, y;
+		Point(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
 	}
-	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int[] point = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		int x = point[0], y = point[1];
-		int[][] A = new int[N][N];
-		for ( int i = 0; i < N; i++ ) {
-			A[i] = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		int[][] M = new int[N][N];
+		for ( int i = 0; i < M.length; i++ ) {
+			M[i] = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 		}
+		int findValue = M[point[0]-1][point[1]-1];
+		int[] dx = {0, 1, 0, -1};
+		int[] dy = {-1, 0, 1, 0};
+		int maxGroupSize = 0;
 		
-		int findValue = A[x - 1][y - 1];
-
-    if (findValue < 0 || findValue > 10) {
-      System.out.println(0);
-      return;
-    }
-
-    int[] dx = {0, 1, 0, -1};
-    int[] dy = {1, 0, -1, 0};
-
-    int answer = 0;
-
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < N; j++) {
-        if (A[i][j] == findValue) {
-          int a = BFS(i, j, N, A, dx, dy, findValue);
-          answer = Math.max(answer, a);
-        }
-      }
-    }
-
-  	System.out.println(answer);
-		/**
-		1. 시작점 찾기
-		2. 시작점의 값 구하기
-		3. 
-		*/
+		for ( int i = 0; i < M.length; i++ ) {
+			for ( int j = 0; j < M[i].length; j++ ) {
+				if ( M[i][j] == findValue ) {
+					int cnt = BFS(i, j, findValue, M, dx, dy);
+					maxGroupSize = Math.max(maxGroupSize, cnt);
+				}
+			}
+		}
+		System.out.println(maxGroupSize);
 	}
-	static int BFS(int x, int y, int N, int[][] A, int[] dx, int[] dy, int findValue) {
-        Queue<Pair> queue = new ArrayDeque<>();
-        queue.add(new Pair(x, y));
-        A[x][y] = -1;
-        int count = 0;
-
-        while (!queue.isEmpty()) {
-            Pair now = queue.poll();
-            count++;
-
-            for (int k = 0; k < 4; k++) {
-                int realX = now.x + dx[k];
-                int realY = now.y + dy[k];
-
-                if (realX >= 0 && realX < N && realY >= 0 && realY < N) {
-                    if (A[realX][realY] == findValue) {
-                        A[realX][realY] = -1;
-                        queue.add(new Pair(realX, realY));
-                    }
-                }
-            }
-        }
-
-        return count;
-    }
+	
+	public static int BFS(int i, int j, int findValue, int[][] M, int[] dx, int[] dy) {
+		Queue<Point> queue = new LinkedList<>();
+		queue.offer(new Point(i,j));
+		M[i][j] = -1;
+		int count = 0;
+		while ( !queue.isEmpty() ) {
+			Point now = queue.poll();
+			count++;
+			for ( int k = 0; k < 4; k++ ) {
+				int x = now.x+dx[k];
+				int y = now.y+dy[k];
+				if ( x >= 0 && x < M.length && y >= 0 && y < M.length ) {
+					if ( M[x][y] == findValue ) {
+						queue.offer(new Point(x,y));
+						M[x][y] = -1;
+					}
+				}
+			}
+		}
+		return count;
+	}
 }
