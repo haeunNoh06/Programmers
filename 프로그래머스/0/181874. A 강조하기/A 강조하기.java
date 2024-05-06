925e2276cb49dcf830fc217d7f6362f9243a865f
Haeun Noh <okhagrace06@naver.com>
2024-05-06 14:34:49 +0900

    [level 0] Title: A 강조하기, Time: 0.06 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,12 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        myString = myString.replaceAll("a", "A");
-        for ( char c : myString.toCharArray() ) {
-            if ( c < 'a' && c != 'A' && c != ' ') {
-                myString = myString.replace(c, (char)(c+32));
-            }
-        }
-        return myString;
+        return myString.toLowerCase().replaceAll("a", "A");
     }
 }
