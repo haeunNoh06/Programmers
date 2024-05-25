b25870dd37b76acc7c210009c8d85a98c9b2df7d
haeun noh <okhagrace06@naver.com>
2024-05-25 23:59:17 +0900

    [level 0] Title: 0 떼기, Time: 6.27 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,13 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        String answer = "";
-        boolean isNotFirstLeft = false;
-        for ( String s : n_str.split("") ) {
-            if ( !s.equals("0") )
-                isNotFirstLeft = true;
-            if ( isNotFirstLeft )
-                answer += s;
-        }
-        return answer;
+        return Integer.parseInt(n_str)+"";
     }
 }
